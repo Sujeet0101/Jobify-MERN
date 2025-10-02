d27743c2933ffc183ad46a8d40aec28c8a79f62f
Sujeet0101 <suj337raut@gmail.com>
2025-10-02 19:00:57 +0545

    add job complete

--- client/src/context/reducers.jsx
+++ client/src/context/reducers.jsx
@@ -1,19 +1,24 @@
 import { act } from "react"
-import { 
-    DISPALY_ALERT, 
-    CLEAR_ALERT, 
-    REGISTER_USER_BEGIN, 
-    REGISTER_USER_SUCCESS, 
-    REGISTER_USER_ERROR,
-    LOGIN_USER_BEGIN,
-    LOGIN_USER_SUCCESS,
-    LOGIN_USER_ERROR,
-    TOGGLE_SIDEBAR,
-    LOGOUT_USER,
-    UPDATE_USER_BEGIN,
-    UPDATE_USER_SUCCESS,
-    UPDATE_USER_ERROR
- } from "./actions" 
+import {
+  DISPALY_ALERT,
+  CLEAR_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+  LOGIN_USER_BEGIN,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
+  TOGGLE_SIDEBAR,
+  LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
+  CLEAR_VALUES,
+  CREATE_JOB_BEGIN,
+  CREATE_JOB_SUCCESS,
+  CREATE_JOB_ERROR,
+} from "./actions"; 
 import { initialState } from "./appContext"
 
 
@@ -148,6 +153,58 @@ const reducer = (state, action) => {
       }
     }
 
+    if(action.type === HANDLE_CHANGE){
+      return {
+        ...state,
+        [action.payload.name]: action.payload.value,
+      }
+    }
+
+    if(action.type === CLEAR_VALUES){
+
+      const initialState = {
+        isEditing: false,
+        editJobId: '',
+        position: '',
+        company: '',
+        jobLocation: state.userLocation,
+        jobType: 'full-time',
+        status: 'pending',
+      }
+
+      return {
+        ...state,
+        ...initialState,
+      }
+    } 
+
+    if (action.type === CREATE_JOB_BEGIN) {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+
+    if (action.type === CREATE_JOB_SUCCESS) {
+      return {
+        ...state,
+        isLoading: false,
+        showAlert: true,
+        alertType: "success",
+        alertText: "New Job Created!",
+      };
+    }
+
+    if (action.type === CREATE_JOB_ERROR) {
+      return {
+        ...state,
+        isLoading: false,
+        showAlert: true,
+        alertType: "danger",
+        alertText: action.payload.msg,
+      };
+    }
+  
      throw new Error ( `no such action: ${action.type}`)
 }
 
